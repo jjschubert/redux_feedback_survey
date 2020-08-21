8b7abc43efa713a6b741f3c830d15d7e7951e8d3
Jenni Schubert <jenni.schubert@gmail.com>
2020-08-21 17:08:22 -0500

    review, display feedback

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Axios from 'axios';
 
 class Review extends Component {
 
-    submitFeedback = () => {
-        console.log('submit');
-    }
+  submitFeedback = () => {
+    console.log('submit');
+    
+  }
 
   render() {
     return (
       <div>
-       
-          <p>Review</p>
-          <button onClick={this.submitFeedback}>Submit</button>
-        
+
+        <h2>Review Your Feedback</h2>
+
+        <p>Feelings: {this.props.reduxState.feedbackReducer.feeling}</p>
+        <p>Understanding: {this.props.reduxState.feedbackReducer.understanding}</p>
+        <p>Support: {this.props.reduxState.feedbackReducer.support}</p>
+        <p>Comments: {this.props.reduxState.feedbackReducer.comments}</p>
+
+        <button onClick={this.submitFeedback}>Submit</button>
+
       </div>
     );
   }
 }
 
-export default Review;
+const mapStateToProps = (reduxState) => {
+  return {
+    reduxState
+  }
+}
+
+export default connect(mapStateToProps)(Review);
