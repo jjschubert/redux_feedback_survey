cdf0514af0630209b7e2ada93d20b5f9945c5693
Jenni Schubert <jenni.schubert@gmail.com>
2020-08-22 13:22:01 -0500

    delete alert, deletealert component

--- src/components/AdminItem/AdminItem.jsx
+++ src/components/AdminItem/AdminItem.jsx
@@ -1,37 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Button from '@material-ui/core/Button'
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
-import axios from 'axios';
-
+import DeleteAlert from './DeleteAlert'
 
 class AdminItem extends Component {
 
-    deleteFeedback = () => {
-        axios({
-            method: 'DELETE',
-            url: `/feedback/${this.props.item.id}`
-        }).then((response) => {
-            //add refresh here
-            this.props.getFeedback();
-        }).catch( error => {
-            console.log('error in DELETE', error);
-        })
-    }
-    
-
     render() {
         return (
-           <>
-                        <TableRow key={this.props.item.id}>
-                            <TableCell>{this.props.item.feeling}</TableCell>
-                            <TableCell>{this.props.item.understanding}</TableCell>
-                            <TableCell>{this.props.item.support}</TableCell>
-                            <TableCell>{this.props.item.comments}</TableCell>
-                            <TableCell><Button onClick={this.deleteFeedback}>Delete</Button></TableCell>
-                        </TableRow>
-                        </>
+            <>
+                <TableRow key={this.props.item.id}>
+                    <TableCell>{this.props.item.feeling}</TableCell>
+                    <TableCell>{this.props.item.understanding}</TableCell>
+                    <TableCell>{this.props.item.support}</TableCell>
+                    <TableCell>{this.props.item.comments}</TableCell>
+                    <TableCell>
+                    <DeleteAlert item={this.props.item} getFeedback={this.props.getFeedback}/> </TableCell>
+                </TableRow>
+            </>
         );
     }
 }
---
+++ src/components/AdminItem/DeleteAlert.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import axios from 'axios';
+
+export default function AlertDialog(props) {
+  const [open, setOpen] = React.useState(false);
+
+  const deleteFeedback = () => {
+      console.log(props.item.id);
+      setOpen(false);
+    axios({
+        method: 'DELETE',
+        url: `/feedback/${props.item.id}`
+    }).then((response) => {
+        //add refresh here
+        setOpen(false);
+        props.getFeedback();
+    }).catch(error => {
+        console.log('error in DELETE', error);
+    })
+}
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Delete
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+    
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Delete this feedback?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={deleteFeedback} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
